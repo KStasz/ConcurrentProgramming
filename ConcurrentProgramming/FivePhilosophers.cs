4cad8d998d780d9b982f97098bf418916bd21aea
Krzysztof Staszek <krzysztofstaszek@hotmail.com>
2021-02-19 19:07:19 +0100

    Changes

--- ConcurrentProgramming/FivePhilosophers.cs
+++ ConcurrentProgramming/FivePhilosophers.cs
@@ -10,64 +10,53 @@ namespace ConcurrentProgramming
 
         }
 
-        private Semaphore[] widelce;  //tablica binarnych semaforów widelce
-        private Semaphore lokaj;  //semafor lokaj, który dopuszcza
-                                  //co najwyżej 4 filozofów do stołu
-        private Thread[] filozofowie; //tablica wątków filozofów
+        private Semaphore[] forks;
+        private Semaphore waiter;  
+        private Thread[] philosophers; 
 
         public void Start()
         {
             FivePhilosophers pf = new FivePhilosophers();
-            pf.Startuj();
-            Thread.Sleep(10000); //czekamy losowy czas
-            pf.Zakoncz(); //przerywamy wątki
+            pf.Begin();
+            Thread.Sleep(10000);
+            pf.Finish();
         }
 
-        /// Konstruktor klasy PieciuFilozofow odpowiedzialny między innymi
-        /// za inicjalizację i nadanie nazw wątkom.
-        /// 
         public FivePhilosophers()
         {
-            widelce = new Semaphore[5];
+            forks = new Semaphore[5];
             for (int i = 0; i < 5; i++)
             {
-                widelce[i] = new Semaphore(1, 1);  //semafory binarne
+                forks[i] = new Semaphore(1, 1);
             }
-            lokaj = new Semaphore(4, 4);
+            waiter = new Semaphore(4, 4);
 
-            filozofowie = new Thread[5];
+            philosophers = new Thread[5];
             for (int i = 0; i < 5; i++)
             {
-                filozofowie[i] = new Thread(new ThreadStart(Filozof)); //tworzymy wątki
-                filozofowie[i].Name = "Filozof " + i; //i nadajemy im nazwy
+                philosophers[i] = new Thread(new ThreadStart(Philosopher));
+                philosophers[i].Name = "Filozof " + i;
             }
 
         }
 
-        /// Funkcja uruchamiająca wszystkie wątki.
-        /// 
-        public void Startuj()
+        public void Begin()
         {
-            foreach (Thread t in filozofowie)
+            foreach (Thread t in philosophers)
             {
                 t.Start();
             }
         }
 
-        /// Funkcja przerywająca wszystkie wątki
-        /// 
-        public void Zakoncz()
+        public void Finish()
         {
-            foreach (Thread t in filozofowie)
+            foreach (Thread t in philosophers)
             {
                 t.Interrupt();
             }
         }
 
-        /// Wątek filozofa, który w nieskończonej pętli myśli przez losowy czas,
-        /// a następnie próbuje zdobyć dwa widelce i przez losowy czas je.
-        /// 
-        public void Filozof()
+        public void Philosopher()
         {
             try
             {
@@ -76,16 +65,16 @@ namespace ConcurrentProgramming
                 while (true)
                 {
                     Console.WriteLine("{0} myśli", Thread.CurrentThread.Name);
-                    Thread.Sleep(rand.Next(100, 500)); //losowy czas myślenia
+                    Thread.Sleep(rand.Next(100, 500));
 
-                    lokaj.WaitOne(); //sprawdzamy, czy możemy usiąść do stołu
-                    widelce[numer].WaitOne(); //bierzemy lewy widelec
-                    widelce[(numer + 1) % 5].WaitOne(); //bierzemy prawy widelec
+                    waiter.WaitOne();
+                    forks[numer].WaitOne();
+                    forks[(numer + 1) % 5].WaitOne();
                     Console.WriteLine("{0} je", Thread.CurrentThread.Name);
-                    Thread.Sleep(rand.Next(100, 300)); //losowy czas jedzenia
-                    widelce[numer].Release(); //odkładamy lewy widelec
-                    widelce[(numer + 1) % 5].Release(); //odkładamy prawy widelec
-                    lokaj.Release(); //dopuszczamy kolejnego filozofa do stołu                    
+                    Thread.Sleep(rand.Next(100, 300));
+                    forks[numer].Release();
+                    forks[(numer + 1) % 5].Release();
+                    waiter.Release();
                 }
             }
             catch (ThreadInterruptedException)
